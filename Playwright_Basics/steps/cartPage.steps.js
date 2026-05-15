91f601fce68facc10cb9cedd013a00272eae3f7e
gowthammuni29 <gowthammuni29@gmail.com>
2026-05-15 12:58:35 +0530

    Code changes
    after custom fixtures. did some code changes

--- Playwright_Basics/steps/cartPage.steps.js
+++ Playwright_Basics/steps/cartPage.steps.js
@@ -1,103 +1,65 @@
-import { Given, When, Then } from '@cucumber/cucumber';
-import {  expect } from '@playwright/test';
-import { getWorld } from './helpers/world.helper.js';
-import logger from '../helpers/logger.js';
+import { When, Then } from '../support/fixtures.js';
 
-
-
-When('adds it to cart with alert message confirmation', async function(){
-    const world = getWorld(this);
-    await world.productDetailPage.clickAddToCart();
-    await world.productDetailPage.ProductAddedAlert();
-    
+When('adds it to cart with alert message confirmation', async ({ productDetailPage }) => {
+    await productDetailPage.clickAddToCart();
+    await productDetailPage.ProductAddedAlert();
 });
 
-When('user navigate to the cart page', async function(){
-    const world = getWorld(this);
-    await world.cartPage.navigateToCartPage();
+When('user navigate to the cart page', async ({ cartPage }) => {
+    await cartPage.navigateToCartPage();
 });
 
-Then('selected items are visible in cart page', async function () {
-    const world= getWorld(this);
-    await world.cartPage.validateAllItemsAreVisible();
+Then('selected items are visible in cart page', async ({ cartPage }) => {
+    await cartPage.validateAllItemsAreVisible();
 });
 
-Then('place order button should be enabled', async function () {
-    const world= getWorld(this);
-    await world.cartPage.expectPlaceOrderEnabled();
+Then('place order button should be enabled', async ({ cartPage }) => {
+    await cartPage.expectPlaceOrderEnabled();
 });
 
-When('user clicked on remove button', async function(){
-    const world= getWorld(this);
-    await world.cartPage.removeItemFromCart(this.expectedDeviceName);
+When('user clicked on remove button', async ({ cartPage, sharedState }) => {
+    await cartPage.removeItemFromCart(sharedState.expectedDeviceName);
 });
 
-Then('device cart should be empty', async function(){
-    const world= getWorld(this);
-    await world.cartPage.ValidateCartIsEmpty();
+Then('device cart should be empty', async ({ cartPage }) => {
+    await cartPage.ValidateCartIsEmpty();
 });
 
-When('user adds the following devices to the cart:', async function (dataTable){
-
-    const world = getWorld(this);
+When('user adds the following devices to the cart:', async ({ homePage, productDetailPage, sharedState }, dataTable) => {
     const devices = dataTable.hashes();
-
-    for (let row of devices){
-        await world.homePage.selectDevice(row.deviceName);
-
-        // const price = await world.productDetailPage.addProductToCartAndGetPrice();
-
-        // world.selectedProductAndPrice.push({
-        //     name: row.deviceName,
-        //     price: price
-        // });
-        await world.productDetailPage.addProductToCart(); // add product to cart
-        world.selectedProduct.push(row.deviceName); 
-        
-
-        await world.productDetailPage.navigateBackHomePage(); // navigate back to home page to select next device
-
+    for (const row of devices) {
+        await homePage.selectDevice(row.deviceName);
+        await productDetailPage.addProductToCart();
+        sharedState.selectedProduct.push(row.deviceName);
+        await productDetailPage.navigateBackHomePage();
     }
 });
 
-Then('cart should display all the products', async function () {
-    const world= getWorld(this);
-    await world.cartPage.ValidateProductInCart(world.selectedProduct);
-    
+Then('cart should display all the products', async ({ cartPage, sharedState }) => {
+    await cartPage.ValidateProductInCart(sharedState.selectedProduct);
 });
 
-Then('cart total price should be equal to the sum of device price', async function(){
-
-    const world= getWorld(this);
-    await world.cartPage.validateDevicePriceWithCartTotal();
+Then('cart total price should be equal to the sum of device price', async ({ cartPage }) => {
+    await cartPage.validateDevicePriceWithCartTotal();
 });
 
-When('user clicked place order button', async function(){
-    const world= getWorld(this);
-    await world.cartPage.clickPlaceOrder();
+When('user clicked place order button', async ({ cartPage }) => {
+    await cartPage.clickPlaceOrder();
 });
 
-When('user clicked purchase without updating the input field an alert should be shown', async function () {
-    const world =getWorld(this);
-    await world.cartPage.clickPurchaseButton();
-    await world.cartPage.submitPurchaseWithoutData();
-    
+When('user clicked purchase without updating the input field an alert should be shown', async ({ cartPage }) => {
+    await cartPage.clickPurchaseButton();
+    await cartPage.submitPurchaseWithoutData();
 });
 
-When('user fills the purchase details with valid information', async function () {
-    const world =getWorld(this);
-    await world.cartPage.fillPurchaseForm();
+When('user fills the purchase details with valid information', async ({ cartPage }) => {
+    await cartPage.fillPurchaseForm();
 });
 
-Then('order should be placed successfully with order confirmation message', async function () {
-    const world =getWorld(this);
-    await world.cartPage.ValidatePurchaseSuccessMessage();
-   
-
+Then('order should be placed successfully with order confirmation message', async ({ cartPage }) => {
+    await cartPage.ValidatePurchaseSuccessMessage();
 });
 
-Then('user completes the purhcase with selecting ok', async function () {
-    const world =getWorld(this);
-    await world.cartPage.clickPurchaseSuccessOKButton();
+Then('user completes the purhcase with selecting ok', async ({ cartPage }) => {
+    await cartPage.clickPurchaseSuccessOKButton();
 });
-
--- Playwright_Basics/steps/common.steps.js
+++ Playwright_Basics/steps/common.steps.js
@@ -1,18 +1,9 @@
-import { Given, When, Then } from '@cucumber/cucumber';
-import { getWorld } from './helpers/world.helper.js';
+import { Given } from '../support/fixtures.js';
 
-
-Given('user has storage state authentication and logged in', async function () {
-  const world = getWorld(this);
-  await world.homePage.openHomePage();
+Given('user has storage state authentication and logged in', async ({ homePage }) => {
+    await homePage.openHomePage();
 });
 
-
-Given('user in on the cart page', async function () {
-  const world = getWorld(this);
-  await world.cartPage.navigateToCartPage();
-  
+Given('user in on the cart page', async ({ cartPage }) => {
+    await cartPage.navigateToCartPage();
 });
-
-
-
